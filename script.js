ed7440be94eae5e20fda743a3700b361757b58ab
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2022-05-31 12:55:36 -0400

    added question changing on start, but timer is broken and stuck

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-// let key;
-// let quiz;
+import * as q from "./quiz.js";
 
-// const fetchKey = async () => {
-//   const res = await fetch("./token.json");
-//   key = await res.json();
-//   key = key.API_TOKEN;
-// }
+const quiz = q.quiz;
+const quizTitleQues = document.querySelector(".quiz_Container p");
+let timer = document.querySelector(".timer");
+const initBtn = document.querySelector(".initBtn");
 
-// const fetchQuiz = async (key) => {
-//   const res = await fetch(`https://quizapi.io/api/v1/questions/apikey?${key}`);
-//   console.log("res", res)
-//   quiz = res;
-// }
+//Random index
+const randIndex = (length) => Math.floor(Math.random() * length);
 
-// fetchKey().then(() => {
-//   console.log(key)
-//   fetchQuiz(key)
-// })
+//Init will:
+// -change the display of .quiz_Container h1 to a random question
+// -change the display of .quiz_ContainerDesc to a button group that are the ans choices
+// -will start the timer
 
+const startTimer = () => {
+  while(timer.innerHTML > 0) {
+    console.log("timer", timer.innerHTML);
 
+    timer.innerHTML = timer.innerHTML--;
+  }
+  clearInterval(startTimer);
+}
+
+initBtn.addEventListener("click", () => {
+  console.log("started");
+  console.log("quizTitleQues", quizTitleQues);
+  console.log("timer", timer);
+  console.log("quiz", quiz.length);
+  console.log("rand", randIndex(quiz.length));
+  //Start timer at 70
+  timer.innerHTML=70;
+  //This will write a random quiz question to the element
+  quizTitleQues.innerHTML=quiz[randIndex(quiz.length)].question;
+  setInterval(startTimer, 1000);
+  console.log("past the interval")
+})
